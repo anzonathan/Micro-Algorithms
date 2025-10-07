9ee9c6f264fafaa92fec9a55d179e55a4704a293
Nathaniel Anzo Mugenyi <41405954+anzonathan@users.noreply.github.com>
2025-10-07 04:35:01 +0300

    Add: numeronym

---
+++ numeronym.java
@@ -0,0 +1,16 @@
+
+public class numeronym{
+
+    public static void main(String[] args){
+
+        String decomp = "internationalization";
+
+        int letterCount = decomp.length();
+
+        System.out.print(decomp.charAt(0));
+        System.out.print(letterCount-2);
+        System.out.print(decomp.charAt(letterCount-1));
+
+        
+    }
+}
